3186f1b76c391b8ac91304c6fbfc10d757f1c759
riley pence <rileypence4@gmail.com>
2020-04-30 15:09:02 -0700

    spelling fix

--- Code_Challenges/two_strings.py
+++ Code_Challenges/two_strings.py
@@ -24,41 +24,6 @@ def twoStrings(s1, s2):
             continue
     print(answer)
 
-
-# Complete the twoStrings function below.
-# def twoStrings(s1, s2):
-#     answer = []
-#     for i in range(0, 2):
-#         action = False
-#         first_str_hash = {}
-#         for c in s1[i]:
-#             if c in first_str_hash:
-#                 first_str_hash[c] += 1
-#             else:
-#                 first_str_hash[c] = 1
-#         for c in s2[i]:
-#             if c in first_str_hash and first_str_hash[c] > 0:
-#                 first_str_hash[c] -= 1
-#                 action = True
-#                 break
-#             else:
-#                 continue
-#         if action == False:
-#             answer.append("NO")
-#         elif action == True:
-#             answer.append("YES")
-#     return answer
-
-
-#But too slow
-# def twoStrings(s1, s2):
-#     for i in range(0,len(s1)):
-#         if(s1[i] in s2):
-#             return "YES"
-#             break
-#     else:
-#         return "NO"
-
 hello = "hello"
 world = "world"
 
